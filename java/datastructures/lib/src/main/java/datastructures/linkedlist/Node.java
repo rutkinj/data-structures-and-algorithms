5f0362d43faca1d35f282345a62a1a18c9fe45ed
Joe Rutkin <rutkinj@gmail.com>
2022-10-28 17:16:47 -0700

    adds linked list, node, and tests

---
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/Node.java
@@ -0,0 +1,12 @@
+package datastructures.linkedlist;
+
+public class Node {
+  // constructor?
+  public String value;
+  public Node next = null;
+
+  public Node(String nodeValue){
+    value = nodeValue;
+  }
+
+}
