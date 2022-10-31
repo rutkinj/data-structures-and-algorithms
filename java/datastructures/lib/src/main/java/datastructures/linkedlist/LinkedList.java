95adab7b54001302b7a4e820073f18ee16eda2bf
Joe Rutkin <rutkinj@gmail.com>
2022-10-31 15:34:56 -0700

    adds lab 06 methods

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -4,6 +4,7 @@ public class LinkedList
 {
   public Node head = null;
   public LinkedList(){}
+
   public void insert(String value){
     Node newHead = new Node(value);
     newHead.next = head;
@@ -20,16 +21,54 @@ public class LinkedList
     }
     return false;
   }
+
   @Override
-  public String toString()
-    {
-      StringBuilder retStr = new StringBuilder();
-      Node current = head;
-      while(current != null){
-        retStr.append("{ " + current.value + " } => ");
-        current = current.next;
-      }
-      retStr.append("NULL");
-      return retStr.toString();
+  public String toString() {
+    StringBuilder retStr = new StringBuilder();
+    Node current = head;
+    while(current != null){
+      retStr.append("{ " + current.value + " } => ");
+      current = current.next;
     }
+    retStr.append("NULL");
+    return retStr.toString();
+  }
+
+  public void append(String value){
+    //add new node to end of ll
+    Node current = head;
+    while(current.next != null){
+      current = current.next;
+    }
+    Node newNode = new Node(value);
+    current.next = newNode;
+  }
+
+  public void insertBefore(String searchValue, String addValue){
+    Node current = head;
+    while(current != null){
+      if (current.next.value == searchValue){
+        Node newNode = new Node(addValue);
+        newNode.next = current.next;
+        current.next = newNode;
+        return;
+      }
+      current = current.next;
+    }
+    System.out.println("Search value not found");
+  }
+
+  public void insertAfter(String searchValue, String addValue){
+    Node current = head;
+    while(current != null){
+      if (current.value == searchValue){
+        Node newNode = new Node(addValue);
+        newNode.next = current.next;
+        current.next = newNode;
+        return;
+      }
+      current = current.next;
+    }
+    System.out.println("Search value not found");
+  }
 }
